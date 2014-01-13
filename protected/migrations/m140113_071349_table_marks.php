b5cf068bcae7057c28a52c60cee542cd5311a08d
Dmytro Karpovych <ZAYEC77@gmail.com>
2014-01-13 09:27:49 +0200

    create table 'mark'

---
+++ protected/migrations/m140113_071349_table_marks.php
@@ -0,0 +1,35 @@
+<?php
+
+class m140113_071349_table_marks extends CDbMigration
+{
+    public function up()
+    {
+        $this->createTable(
+            'mark',
+            array(
+                'id' => 'pk',
+                'student_id' => 'integer NOT NULL',
+                'load_id' => 'integer NOT NULL',
+                'date' => 'date NOT NULL',
+                'value' => 'byte NOT NULL',
+            )
+        );
+    }
+
+    public function down()
+    {
+        $this->dropTable('mark');
+        echo "success." . PHP_EOL;
+    }
+
+    /*
+    // Use safeUp/safeDown to do migration with transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
