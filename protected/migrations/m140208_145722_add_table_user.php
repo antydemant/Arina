514120bda82444113c96f4177e9f15d698577296
Dmytro Karpovych <ZAYEC77@gmail.com>
2014-02-08 17:10:35 +0200

    tbl user

---
+++ protected/migrations/m140208_145722_add_table_user.php
@@ -0,0 +1,36 @@
+<?php
+
+class m140208_145722_add_table_user extends CDbMigration
+{
+	public function up()
+	{
+        $this->createTable(
+            'user',
+            array(
+                'id' => 'pk',
+                'username' => 'string NOT NULL',
+                'password' => 'string NOT NULL',
+                'email' => 'string NOT NULL',
+                'role' => 'integer',
+                'identity_id' => 'integer',
+            )
+        );
+	}
+
+	public function down()
+	{
+        $this->dropTable('user');
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
