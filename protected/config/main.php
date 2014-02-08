d73a9dc5806a4b64d4f6ffe61121e7f254f5d739
Dmytro Karpovych <ZAYEC77@gmail.com>
2014-02-08 18:23:52 +0200

    users CRUD

--- protected/config/main.php
+++ protected/config/main.php
@@ -1,10 +1,7 @@
 <?php
-
-// uncomment the following to define a path alias
-// Yii::setPathOfAlias('local','path/to/local-folder');
-
-// This is the main Web application configuration. Any writable
-// CWebApplication properties can be configured here.
+/**
+ *
+ */
 return CMap::mergeArray(
     array(
         'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
