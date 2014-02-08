38593260808ae98aea3dabe3e31b818e8be60d81
Dmytro Karpovych <ZAYEC77@gmail.com>
2014-02-08 18:59:47 +0200

    user login link fix

--- protected/components/Booster.php
+++ protected/components/Booster.php
@@ -6,6 +6,7 @@ class Booster {
     const GRID_VIEW = "bootstrap.widgets.TbGridView";
     const BUTTON_GROUP = "bootstrap.widgets.TbButtonGroup";
     const FORM = "bootstrap.widgets.TbActiveForm";
+    const NAVIGATOR = "bootstrap.widgets.TbNavbar";
 
     /**
      * Control's types
