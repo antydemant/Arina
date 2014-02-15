21a9ed2a8adcd7a0a731056a988c53088243dca9
Serhiy Vinichuk <serhiyvinichuk@gmail.com>
2014-02-15 15:28:57 +0200

    fix #1

--- protected/modules/studyPlan/views/plan/create.php
+++ protected/modules/studyPlan/views/plan/create.php
@@ -1,8 +1,11 @@
 <?php
 /**
- *
  * @var PlanController $this
  * @var Plan $model
  */
+$this->breadcrumbs = array(
+    'Навчальні плани'=>$this->createUrl('index'),
+    'Новий навчальний план'
+);
 ?>
 <?php $this->renderPartial('_form', array('model' => $model)); ?>
