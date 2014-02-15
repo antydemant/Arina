2f661dea967018b5bd67b91a8e3cb3ce393ae6c3
Serhiy Vinichuk <serhiyvinichuk@gmail.com>
2014-02-15 19:11:17 +0200

    start ajax form

--- protected/modules/studyPlan/controllers/DefaultController.php
+++ protected/modules/studyPlan/controllers/DefaultController.php
@@ -2,87 +2,18 @@
 
 class DefaultController extends Controller
 {
-	public function actionIndex()
-	{
-		$this->render('index');
-	}
-}
-/*
- * <?php
-
-protected function beforeAction($action)
-{
-    return parent::beforeAction($action);
-}
-
-public function actionIndex()
-{
-    $provider = StudyPlan::model()->getProvider();
-    $this->render(
-        'index',
-        array(
-            'dataProvider' => $provider,
-        )
-    );
-}
-
-public function actionCreate()
-{
-    $model = new StudyPlan();
-
-    $this->ajaxValidation('group-form', $model);
-
-    if (isset($_POST['StudyPlan'])) {
-        $model->attributes = $_POST['StudyPlan'];
-        if ($model->save()) {
-            $this->redirect(array('/studyPlan/semesters/' . $model->id));
-        }
+    public function actionIndex()
+    {
+        $this->render('index');
     }
 
-
-    $this->render(
-        'create',
-        array(
-            'model' => $model,
-        )
-    );
-}
-
-public function actionUpdateSemesters()
-{
-    $model = new StudyPlan();
-
-    $this->render(
-        'updateSemesters',
-        array(
-            'model' => $model,
-        )
-    );
-}
-
-public function actionSemesters($id)
-{
-    $params = array(
-        'criteria' => array(
-            'condition' => "study_plan_id =$id",
-        ),
-    );
-    $dataProvider = StudyPlanInfo::model()->getProvider($params);
-
-    $this->render(
-        'semesters',
-        array(
-            'dataProvider' => $dataProvider,
-        )
-    );
-}
-
-public function actionDelete($id)
-{
-    $model = StudyPlan::model()->loadContent($id);
-    if ($model->delete() && $model->save()) {
-        $this->redirect(array('studyPlan/index'));
+    public function actionPlan($id)
+    {
+        if (Yii::app()->request->isAjaxRequest) {
+            echo TbHtml::dropDownList('plan', '', TbHtml::listData(Plan::model()->findAll('speciality_id=' . $id), 'id', 'study_year'),
+                array('empty' => 'Оберіть навчальний план'));
+            Yii::app()->end();
+        } else
+            throw new CHttpException(400);
     }
-
 }
-}*/
