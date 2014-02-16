9937be66d17c63f0247ce56304bf559c8878cd2e
CyrusVorazan <cyrusvorazan@gmail.com>
2014-02-16 21:53:39 +0200

    CRUDs for classes, marks and absences

---
+++ protected/views/actualClass/_view.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $this ActualClassController */
+/* @var $data ActualClass */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('date')); ?>:</b>
+	<?php echo CHtml::encode($data->date); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('class_number')); ?>:</b>
+	<?php echo CHtml::encode($data->class_number); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('teacher_load_id')); ?>:</b>
+	<?php echo CHtml::encode($data->teacher_load_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('class_type')); ?>:</b>
+	<?php echo CHtml::encode($data->class_type); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('group_id')); ?>:</b>
+	<?php echo CHtml::encode($data->group_id); ?>
+	<br />
+
+
+</div>
