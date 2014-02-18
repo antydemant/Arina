9c9b463e2e71dbf2692497814786e8a9a8b89701
CyrusVorazan <cyrusvorazan@gmail.com>
2014-02-18 20:41:09 +0200

    module shenanigans

--- protected/modules/journal/views/default/index.php
+++ protected/modules/journal/views/default/index.php
@@ -7,6 +7,39 @@ $this->breadcrumbs=array(
 ?>
 <h1><?php echo $this->uniqueId . '/' . $this->action->id; ?></h1>
 
+<?php
+
+$this->widget(
+		'bootstrap.widgets.TbExtendedGridView',
+		array(
+				'type' => 'striped bordered',
+				'dataProvider' => new CActiveDataProvider('ClassMark', array(
+						'criteria' => array(
+								'limit' => 10,
+								'order' => 'RAND()'
+						)
+				)),
+				'template' => "{items}",
+				'columns' => array(
+						array(
+								'name' => 'id',
+								'header' => '#',
+								'htmlOptions' => array('style' => 'width: 60px')
+						),
+						array('name' => 'country_code', 'header' => '19.06'),
+						array(
+								'htmlOptions' => array('nowrap' => 'nowrap'),
+								'class' => 'bootstrap.widgets.TbButtonColumn',
+								'viewButtonUrl' => null,
+								'updateButtonUrl' => null,
+								'deleteButtonUrl' => null,
+						)
+				)
+		)
+);
+
+?>
+
 <p>
 This is the view content for action "<?php echo $this->action->id; ?>".
 The action belongs to the controller "<?php echo get_class($this); ?>"
