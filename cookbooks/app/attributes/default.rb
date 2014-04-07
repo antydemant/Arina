2bf636e31f377a674a76a5f022b59bf183987ac2
Volodymyr Gamula <v.gamula@gmail.com>
2014-04-07 18:15:57 +0300

    Changed project structure (goodbye vagrant)

--- cookbooks/app/attributes/default.rb
+++
@@ -1,7 +0,0 @@
-default["vhosts"]["app"]["alias_path"] = ""
-default["vhosts"]["app"]["database"] = "app"
-default["vhosts"]["app"]["path"] = "/var/www"
-default["vhosts"]["app"]["docroot"] = "/var/www/src"
-default["vhosts"]["app"]["server_name"] = "vm-app"
-default["vhosts"]["app"]["server_email"] = "vm.app@example.com"
-default["vhosts"]["app"]["composer_need"] = false
