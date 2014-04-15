c538ad3deb1ba2a6673268da4ca4d5e577ad502d
Volodymyr Gamula <v.gamula@gmail.com>
2014-04-15 15:34:20 +0300

    Added very important file from study plan module

---
+++ protected/modules/studyPlan/StudyPlanModule.php
@@ -0,0 +1,27 @@
+<?php
+ 
+/**
+ * @author Serhiv Vinichuk <serhiyvinichuk@gmail.com>
+ * Class StudyPlanModule
+ */
+class StudyPlanModule extends CWebModule
+{
+    public $defaultController = 'main';
+
+    public function init()
+    {
+        $this->setImport(array(
+            'studyPlan.models.*',
+            'studyPlan.components.*',
+        ));
+
+    }
+
+    public function beforeControllerAction($controller, $action)
+    {
+        if (parent::beforeControllerAction($controller, $action)) {
+            return true;
+        } else
+            return false;
+    }
+}
