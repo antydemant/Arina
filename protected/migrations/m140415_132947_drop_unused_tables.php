a2234fe766e3973dd1828ec045037e5833d0dc56
Volodymyr Gamula <v.gamula@gmail.com>
2014-04-16 00:19:03 +0300

    Added one more migration for unused tables

---
+++ protected/migrations/m140415_132947_drop_unused_tables.php
@@ -0,0 +1,35 @@
+<?php
+
+class m140415_132947_drop_unused_tables extends CDbMigration
+{
+	public function up()
+	{
+        if (Yii::app()->getDb()->schema->getTable('study_plan') !== null) {
+            $this->dropTable('study_plan');
+        }
+        if (Yii::app()->getDb()->schema->getTable('study_plan') !== null) {
+            $this->dropTable('study_plan');
+        }
+        if (Yii::app()->getDb()->schema->getTable('study_plan_subject') !== null) {
+            $this->dropTable('study_plan_subject');
+        }
+
+	}
+
+	public function down()
+	{
+		echo "m140415_132947_drop_unused_tables does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
