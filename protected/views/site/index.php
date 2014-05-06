1434153240851c6618b2e95c332da814931627ae
CyrusVorazan <cyrusvorazan@gmail.com>
2014-05-07 02:15:14 +0300

    And a quick fix

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -2,11 +2,13 @@
 /* @var $this SiteController */
 
 $this->pageTitle = Yii::app()->name;
+/*
 if (isset(Yii::app()->user->identityId)) {
     echo 'Id -> ' . Yii::app()->user->getId() . '<br>';
     echo 'Identity id -> ' . Yii::app()->user->identityId . '<br>';
     echo 'Identity type -> ' . Yii::app()->user->identityType . '<br>';
 }
+*/
 ?>
 
 <h1>Вітаємо вас в<i> <?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
