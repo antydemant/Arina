c8d966627c9471abffec9c5988b26a52836ca393
Kris <elhowm@gmail.com>
2014-05-20 11:34:54 +0300

    new_student_questionary

--- public/index.php
+++ public/index.php
@@ -2,8 +2,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 date_default_timezone_set(date_default_timezone_get());
+$env = (isset($_SERVER['APP_ENV'])) ? $_SERVER['APP_ENV'] : 'prod';
+
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_DEBUG') or define('YII_DEBUG', ($env == 'dev'));
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
