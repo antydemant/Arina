2133afd9015d5a464b0f3ca3aed2daff663bebcf
Volodymyr Gamula <v.gamula@gmail.com>
2014-06-10 21:40:33 +0300

    removed files

--- protected/data/authassignment.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `authassignment` (`itemname`, `userid`, `bizrule`, `data`) VALUES
-('admin', '1', NULL, 'N;'),
-('curator', '92', NULL, 'N;'),
-('dephead', '91', NULL, 'N;'),
-('prefect', '95', NULL, 'N;'),
-('student', '94', NULL, 'N;'),
-('teacher', '93', NULL, 'N;');
