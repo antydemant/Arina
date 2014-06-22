e2a31916879de8c9635b560853e952cf50d468db
Dmytro Karpovych <ZAYEC77@gmail.com>
2014-06-22 18:21:48 +0300

    main menus by roles

---
+++ protected/components/menu/dephead.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *
+ * @author Dmytro Karpovych <ZAYEC77@gmail.com>
+ */
+$menu = array(
+    array('label' => Yii::t('base', 'Groups'), 'url' => array('/group')),
+    array('label' => Yii::t('base', 'Study plans'), 'url' => array('/studyPlan')),
+    array('label' => Yii::t('base', 'Load'), 'url' => array('/load')),
+    array('label' => Yii::t('base', 'Students'), 'url' => array('/student')),
+    array('label' => Yii::t('base', 'Journal'), 'url' => array('/journal')),
+    array('label' => Yii::t('base', 'File Shares'), 'url' => array('/fileShare')),
+);
